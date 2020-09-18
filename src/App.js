f769365e8a84873752b8b82164e3dda80118d858
iona-b <ionabrabender@gmail.com>
2020-09-18 16:02:02 -0700

    added initial css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="h1" >Iona Brabender</h1>
-      <h2>Full-Stack Software Engineer | Ruby on Rails, JavaScript, React, Redux</h2>
-      <a href="https://github.com/iona-b">github</a><br></br>
-      <a href="https://www.linkedin.com/in/iona-brabender">LinkedIn</a><br></br>
-      <a href="https://dev.to/ionab">Blog</a>
+    <div className="homepage">
+      <div id="upper-navbar">
+        <h1 className="h1" >Iona Brabender</h1>
+        <h2 className="h2" >Full-Stack Software Engineer | Ruby on Rails, JavaScript, React, Redux</h2>
+      </div>
+      <div id="navbar">
+        <a href="https://github.com/iona-b" className="navbar-links" >github</a><br></br>
+        <a href="https://www.linkedin.com/in/iona-brabender" className="navbar-links" >LinkedIn</a><br></br>
+        <a href="https://dev.to/ionab" className="navbar-links" >Blog</a>
+      </div>      
       <h1 id="under-construction" >Under Construction</h1>
     </div>
   );

ed9be120877b3d0884205722a06e047623f367ba
iona-b <ionabrabender@gmail.com>
2020-09-18 14:38:40 -0700

    Updates

---
+++ precache-manifest.575602fae74ddb7d119d70a2dc4cbc79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5aa067ee8123fad38825c6c13fec7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7604e13eec9ce2b8f05b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3990a172276b1b411235",
+    "url": "/static/js/2.9e493013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e493013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7604e13eec9ce2b8f05b",
+    "url": "/static/js/main.e3c92c51.chunk.js"
+  },
+  {
+    "revision": "237e891a3ffba5731789",
+    "url": "/static/js/runtime-main.b6e4a93e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

c5884dc546a0342c91d05cc80d33b341ce416222
iona-b <ionabrabender@gmail.com>
2020-09-18 15:05:58 -0700

    under construction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Iona Brabender</h1>
+      <h1 className="h1" >Iona Brabender</h1>
+      <h2>Full-Stack Software Engineer | Ruby on Rails, JavaScript, React, Redux</h2>
+      <a href="https://github.com/iona-b">github</a><br></br>
+      <a href="https://www.linkedin.com/in/iona-brabender">LinkedIn</a><br></br>
+      <a href="https://dev.to/ionab">Blog</a>
+      <h1 id="under-construction" >Under Construction</h1>
     </div>
   );
 }

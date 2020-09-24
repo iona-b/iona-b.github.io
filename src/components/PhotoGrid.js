33d8e7aeaf268116e13466ad6195913a2f2230a1
iona-b <ionabrabender@gmail.com>
2020-09-24 14:12:54 -0700

    added photo grid

---
+++ src/components/PhotoGrid.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function PhotoGrid() {
+
+  return (
+
+    <div className="components" id="photo-grid-div" >
+        {/* Need to get Scotland image */}
+        <img src={require("../media/ghent.jpeg")} alt='' className="photo-grid-photos" />
+        <img src={require("../media/ghent.jpeg")} alt='' className="photo-grid-photos" />
+        <img src={require("../media/france.jpeg")} alt='' className="photo-grid-photos" />
+        <img src={require("../media/golden-gate-bridge.jpeg")} alt='' className="photo-grid-photos" />
+        <img src={require("../media/new-york.jpeg")} alt='' className="photo-grid-photos" />
+        <img src={require("../media/tahoe.jpeg")} alt='' className="photo-grid-photos" />
+        <img src={require("../media/bodega-bay.jpeg")} alt='' className="photo-grid-photos" />
+        <img src={require("../media/pavlova.jpeg")} alt='' className="photo-grid-photos" />
+    </div>
+    
+  );
+  
+}
+
+export default PhotoGrid;
--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import About from '../components/About'
+import PhotoGrid from '../components/PhotoGrid'
 
-function AboutContainer() {
+class AboutContainer extends React.Component {
 
-  return (
+  state = {
+    showing: "About"
+  }
 
-    <div className="containers" id="about-container">
-        <h2 className="h2" >Hello!</h2>
-        <About />
-    </div>
-    
-  );
+  handleShowingChange = (event) => {
+    this.setState ({
+      showing: event.target.name
+    })
+    console.log(this.state)
+  }
+
+  render() {
+
+    return (
+
+      <div className="containers" id="about-container">
+          <h2 className="h2" >Hello!</h2>
+          {this.state.showing === "About"
+            ?
+            <div>
+              <button className="buttons about-buttons" name="More" onClick={this.handleShowingChange} >More</button>
+              <About />
+            </div>
+            : 
+            <div>
+              <button className="buttons about-buttons" name="About" onClick={this.handleShowingChange}  >About</button>
+              <PhotoGrid />
+            </div>
+          }
+      </div>
+      
+    );
+
+  }
+
   
 }
 

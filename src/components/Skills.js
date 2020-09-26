86be7028ff55a1bf28c2e0fdcf26b0ed45f64881
iona-b <ionabrabender@gmail.com>
2020-09-26 15:18:22 -0700

    added skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,63 @@ function Skills() {
   return (
 
     <div className="one-second-animation" >
+
         <h2>Skills</h2>
+
+        <div id="skills-div">
+
+          <div className=" skills-divs" >
+            
+              <h2 className="h2-standard" >Technical</h2>
+                <div className="resume-bullet-points-div" >
+                  <p className="resume-bullet-points" >• Ruby</p>
+                  <p className="resume-bullet-points" >• Ruby on Rails</p>
+                  <p className="resume-bullet-points" >• SQL</p>
+                  <p className="resume-bullet-points" >• JavaScript</p>
+                  <p className="resume-bullet-points" >• React</p>
+                  <p className="resume-bullet-points" >• Redux</p>
+                  <p className="resume-bullet-points" >• HTML</p>
+                  <p className="resume-bullet-points" >• CSS</p>
+                </div>
+
+            </div>
+
+            <div className=" skills-divs" >
+            
+              <h2 className="h2-standard" >Interpersonal</h2>
+                <div className="resume-bullet-points-div" >
+                  <p className="resume-bullet-points" >• Teamwork</p>
+                  <p className="resume-bullet-points" >• Communication</p>
+                  <p className="resume-bullet-points" >• Customer Service</p>
+                </div>
+
+            </div>
+
+            <div className=" skills-divs" >
+            
+              <h2 className="h2-standard" >Languages</h2>
+                <div className="resume-bullet-points-div" >
+                  <p className="resume-bullet-points" >• English: Native Speaker</p>
+                  <p className="resume-bullet-points" >• Dutch: Fluent</p>
+                  <p className="resume-bullet-points" >• French: Basic</p>
+                </div>
+
+            </div>
+
+            <div className=" skills-divs" >
+            
+              <h2 className="h2-standard" >Other</h2>
+                <div className="resume-bullet-points-div" >
+                  <p className="resume-bullet-points" >• Research</p>
+                  <p className="resume-bullet-points" >• Writing</p>
+                  <p className="resume-bullet-points" >• Editing</p>
+                  <p className="resume-bullet-points" >• Event Planning</p>
+                </div>
+
+            </div>
+
+          </div>
+
     </div>
     
   );

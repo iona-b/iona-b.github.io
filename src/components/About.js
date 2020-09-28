8ee0760dd4e2c5ee0b40a1b2b0d2400afd596fc1
iona-b <ionabrabender@gmail.com>
2020-09-28 15:53:42 -0700

    updated social links

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
 
     <div className="components" id="about-div">
       <img src={require("../media/iona.png")} alt='' id="profile-picture" />
-      <p className="p" id="about" >I'm Iona, a recent graduate from the immersive Software Engineering program at Flatiron School. </p> 
+      <p className="p" id="about" >I'm Iona, a recent graduate from the immersive Software Engineering program at Flatiron School. </p>
       <p className="p" id="about" >I think I've always been a coder at heart - I love being able to combine my creative side with my problem solving skills. </p> 
       <p className="p" id="about" >Having the chance to learn and develop myself is what motivates me more than anything else and my varied background gives me a unique perspective in all of my coding projects.</p> 
     </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
   return (
 
     <div className="components one-second-animation" >
-      <p className="p" >I'm currently open to work, so please reach out if you are looking for an enthusiastic and creative developer! </p><br></br>
+      <p className="p" >I'm currently looking for opportunities in the San Francisco Bay Area, so please reach out if you are looking for an enthusiastic and creative developer! </p><br></br>
       <div id="contact-div">
         <a href="https://github.com/iona-b" rel="noopener noreferrer" target="_blank" >
           <img src={require("../media/github-icon.svg")} alt='' className="contact-icons" />

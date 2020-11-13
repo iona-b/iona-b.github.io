8c8b0de69d7298e543fddc65d765ba4737e21d87
iona-b <ionabrabender@gmail.com>
2020-11-13 11:35:01 -0800

    added live demo

--- src/components/AlwaysBalanced.js
+++ src/components/AlwaysBalanced.js
@@ -25,7 +25,10 @@ class AlwaysBalanced extends React.Component {
             <p className="p" id="about"  >Always Balanced is designed to improve your work-life balance while working from home. By simply adding a few details about yourself, you can create a personalised schedule which reflects how you like to work as well as how you like to relax. In today's world, it is vital that you take the time to delineate between work life and personal life, and Always Balanced aims to help you do just that.</p>
             <p className="p" id="about" >When creating a schedule, you can add tasks from your pre-existing tasks list, make new tasks, and select the relaxation categories you're interested in. We'll then generate a schedule for you, inclusive of the tasks you want to tackle, short breaks, longer breaks with suggested relaxation activities, and time off for lunch. By following this plan, you'll have ample time for both work and relaxation, and hopefully experience a better work-life balance.</p>
             <div>
-              <button className="buttons small-buttons" onClick={this.handleChangeShowing} >Demo</button>
+              <button className="buttons small-buttons" onClick={this.handleChangeShowing} >Video</button>
+              <a href="https://always-balanced-frontend.herokuapp.com/"  rel="noopener noreferrer" target="_blank" >
+                <button className="buttons small-buttons" >Demo</button>
+              </a>
               <a href="https://github.com/iona-b/always-balanced-frontend"  rel="noopener noreferrer" target="_blank" >
                 <button className="buttons small-buttons" >GitHub</button>
               </a>

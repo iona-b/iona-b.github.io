e500043037532d5bd0be536da1a87382a9e8d355
iona-b <ionabrabender@gmail.com>
2021-01-06 16:49:06 -0800

    fixed socials overlapping

--- src/App.js
+++ src/App.js
@@ -32,54 +32,48 @@ class App extends React.Component {
 
   render() {
 
-    window.addEventListener('resize', this.handleResize)
-
     return (
 
-      <div>
-
-        <div className="homepage">
-    
-          <div id="navbar">   
-            <NavLink to='/' exact className="navbar-links" >
-                <img src={require("./media/logo.png")} alt='' id="logo" />
-            </NavLink>     
-            <NavLink to='/contact' exact className="navbar-links" >05. Contact</NavLink>
-            <NavLink to='/about' exact className="navbar-links" onClick={this.handleForceUpdateTrue} >04. About</NavLink>
-            <NavLink to='/resume' exact className="navbar-links" onClick={this.handleForceUpdateTrue} >03. Résumé</NavLink>
-            <NavLink to='/projects' exact className="navbar-links" onClick={this.handleForceUpdateTrue} >02. Projects</NavLink>
-            <NavLink to='/' exact className="navbar-links" >01. Home</NavLink>
-          </div >
-    
-          <div id="social-links-div">
-            <a href="https://github.com/iona-b" rel="noopener noreferrer" target="_blank" >
-              <img src={require("./media/github-icon.svg")} alt='' className="icons" />
-            </a><br></br>
-            <a href="https://www.linkedin.com/in/iona-brabender" rel="noopener noreferrer" target="_blank" >
-              <img src={require("./media/linkedin-icon.svg")} alt='' className="icons" />
-            </a><br></br>
-            <a href="https://dev.to/ionab" rel="noopener noreferrer" target="_blank" >
-              <img src={require("./media/dev.to-icon.svg")} alt='' className="icons" />
-            </a><br></br>
-            <a href="https://medium.com/@ionabrabender" rel="noopener noreferrer" target="_blank" >
-              <img src={require("./media/medium-icon.png")} alt='' className="icons" />
-            </a><br></br>
-            <a href={`mailto:ionabrabender@gmail.com`} >
-              <img src={require("./media/send.png")} alt='' className="icons" />
-            </a>
-          </div>     
-    
-          <Switch>
-            <Route exact path='/' component={HomeContainer} />
-            <Route exact path='/about' component={this.renderAboutContainer} />
-            <Route exact path='/projects' component={this.renderProjectsContainer} />
-            <Route exact path='/resume' component={this.renderResumeContainer} />
-            <Route exact path='/contact' component={Contact} />
-            <Route component={NotFound}/>
-          </Switch>
-    
-        </div>
-
+      <div id="homepage">
+  
+        <div id="navbar">   
+          <NavLink to='/' exact className="navbar-links" >
+              <img src={require("./media/logo.png")} alt='' id="logo" />
+          </NavLink>     
+          <NavLink to='/contact' exact className="navbar-links" >05. Contact</NavLink>
+          <NavLink to='/about' exact className="navbar-links" onClick={this.handleForceUpdateTrue} >04. About</NavLink>
+          <NavLink to='/resume' exact className="navbar-links" onClick={this.handleForceUpdateTrue} >03. Résumé</NavLink>
+          <NavLink to='/projects' exact className="navbar-links" onClick={this.handleForceUpdateTrue} >02. Projects</NavLink>
+          <NavLink to='/' exact className="navbar-links" >01. Home</NavLink>
+        </div >
+  
+        <div id="social-links-div">
+          <a href="https://github.com/iona-b" rel="noopener noreferrer" target="_blank" >
+            <img src={require("./media/github-icon.svg")} alt='' className="icons" />
+          </a><br></br>
+          <a href="https://www.linkedin.com/in/iona-brabender" rel="noopener noreferrer" target="_blank" >
+            <img src={require("./media/linkedin-icon.svg")} alt='' className="icons" />
+          </a><br></br>
+          <a href="https://dev.to/ionab" rel="noopener noreferrer" target="_blank" >
+            <img src={require("./media/dev.to-icon.svg")} alt='' className="icons" />
+          </a><br></br>
+          <a href="https://medium.com/@ionabrabender" rel="noopener noreferrer" target="_blank" >
+            <img src={require("./media/medium-icon.png")} alt='' className="icons" />
+          </a><br></br>
+          <a href={`mailto:ionabrabender@gmail.com`} >
+            <img src={require("./media/send.png")} alt='' className="icons" />
+          </a>
+        </div>     
+  
+        <Switch>
+          <Route exact path='/' component={HomeContainer} />
+          <Route exact path='/about' component={this.renderAboutContainer} />
+          <Route exact path='/projects' component={this.renderProjectsContainer} />
+          <Route exact path='/resume' component={this.renderResumeContainer} />
+          <Route exact path='/contact' component={Contact} />
+          <Route component={NotFound}/>
+        </Switch>
+  
       </div>
     
     );
